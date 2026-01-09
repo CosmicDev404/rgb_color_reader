26f9ef42e65260ddae509a0638450753da1b72c4
Agrim Sharma <agrim.sharma0007@gmail.com>
2026-01-09 09:30:12 +0530

    Added the FIRMAWARE

---
+++ Firmware.ino
@@ -0,0 +1,216 @@
+const int r = 3;
+const int g = 5;
+const int b = 6;
+const int g_i = 200;
+const int b_i = 255;
+const int r_i = 179;
+const int ldr = A0;
+const int ill_butt = 4;
+int val_R = 0;
+int val_G = 0;
+int val_B = 0;
+int avg[3] = {0,0,0};
+bool illuminate = false;
+int ill_butt_state = 0;
+
+float minimum;
+
+void setup() {
+  pinMode(r, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(ldr, INPUT);
+  pinMode(ill_butt, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (digitalRead(ill_butt) == LOW){
+    if (ill_butt_state == 0){
+      ill_butt_state = 1;
+    } else if(ill_butt_state == 1){
+      ill_butt_state = 0;
+    }
+    delay(450);
+  }
+  if (ill_butt_state == 1){
+    illuminate = true;
+  }else{
+    illuminate = false;
+  }
+  if (illuminate){
+    analogWrite(r, r_i);
+    analogWrite(g, g_i);
+    analogWrite(b, b_i);
+  } else{
+    processRGB();
+    Serial.println(avg[0]); //RED
+    Serial.println(avg[1]); //GREEN
+    Serial.println(avg[2]); //BLUE
+    Serial.println(color_mapper(0, int(avg[0]), int(avg[1]), int(avg[2])));  //color name
+    Serial.println(minimum); 
+    Serial.println(color_mapper(1, int(avg[0]), int(avg[1]), int(avg[2])));  //color name
+    Serial.println(minimum);
+  }  
+}
+
+void emit(int color, int intensity, int &val){  //just emits each color and read the LDR values
+  analogWrite(color, intensity);
+  delay(50);
+  val = analogRead(ldr);
+  analogWrite(color, 0);
+  delay(50);
+}
+
+void sample(){  //emits and store the readings for RGB
+  emit(r, r_i, val_R);
+  emit(g, g_i, val_G);
+  emit(b, b_i, val_B);
+}
+
+void processRGB(){  //normalizes RGB readings and maps them to same intensity for further processing
+  int RED = 0;
+  int GREEN = 0;
+  int BLUE = 0;
+  for(int i = 0; i<5; i++){
+    sample();
+    RED = val_R + RED;
+    GREEN = val_G + GREEN;
+    BLUE = val_B + BLUE;
+  }
+  RED = RED / 5;
+  GREEN = GREEN / 5;
+  BLUE = BLUE/ 5;
+  int RED_norm = map(RED, 0, 467, 0, 255);  //fine tuned by from the reflection of a white surface
+  int GREEN_norm = map(GREEN, 0, 229, 0, 255);  //fine tuned by from the reflection of a white surface
+  int BLUE_norm = map(BLUE, 0, 195, 0, 255);  //fine tuned by from the reflection of a white surface
+  avg[0] = RED_norm;
+  avg[1] = GREEN_norm;
+  avg[2] = BLUE_norm;
+}
+
+char* color_mapper(int mode, int R, int G, int B) { //passes color names for inputted RGB codes
+  static char color[50];
+  //float minimum;
+  float red;
+  float green;
+  float blue;
+  float cyan;
+  float magenta;
+  float yellow;
+  float orange;
+  float kesari; //the Indian Saffron
+  float Lgreen;
+  float saffron;
+  float pink;
+  float wheat;
+  float gold;
+  float turquoise;
+  float white;
+  float gray;
+  float teal;
+  float brown;
+  float plotR[3] = {255-R, G, B};
+  avg_arr(plotR, red);
+  float plotG[3] = {R, 255-G, B};
+  avg_arr(plotG, green);
+  float plotB[3] = {R, G, 255-B};
+  avg_arr(plotB, blue);
+  float plotC[3] = {R, 255-G, 255-B};
+  avg_arr(plotC, cyan);
+  float plotM[3] = {255-R, G, 255-B};
+  avg_arr(plotM, magenta);
+  float plotY[3] = {255-R, 255-G, B};
+  avg_arr(plotY, yellow);
+  float plotO[3] = {255-R, abs(165-G), B};
+  avg_arr(plotO, orange);
+  float plotK[3] = {255-R, abs(119-G), abs(34-B)};
+  avg_arr(plotK, kesari);
+  float plotLG[3] = {abs(50-R), abs(205-G), abs(50-B)};
+  avg_arr(plotLG, Lgreen);
+  float plotSaffron[3] = {abs(255-R), abs(183-G), abs(206-B)};
+  avg_arr(plotSaffron, saffron);
+  float plotpink[3] = {abs(251-R), abs(116-G), abs(168-B)};
+  avg_arr(plotpink, pink);
+  float plotwheat[3] = {abs(245-R), abs(222-G), abs(179-B)};
+  avg_arr(plotwheat, wheat);
+  float plotgold[3] = {abs(229-R), abs(184-G), abs(11-B)};
+  avg_arr(plotgold, gold);
+  float plottur[3] = {abs(4-R), abs(255-G), abs(247-B)};
+  avg_arr(plottur, turquoise);
+  float plotwhite[3] = {abs(255-R), abs(255-G), abs(255-B)};
+  avg_arr(plotwhite, white);
+  float plotgray[3] = {abs(128-R), abs(128-G), abs(128-B)};
+  avg_arr(plotgray, gray);
+  float plotteal[3] = {R, abs(128-G), abs(128-B)};
+  avg_arr(plotteal, teal);
+  float plotbrown[3] = {abs(143-R), abs(98-G), abs(81-B)};
+  avg_arr(plotbrown, brown);
+
+  if (mode==0){
+    float val_array[6]= {red, blue, green, cyan, magenta, yellow};
+    min_finder(val_array, 6, minimum);
+  }
+  if (mode==1){
+    float val_array[12]= {orange, kesari, Lgreen, saffron, pink, wheat, gold, turquoise, white, gray, teal, brown};
+    min_finder(val_array, 12, minimum);
+  }
+
+
+  if(minimum==cyan){  // CMY
+    strcpy(color, "Cyan");
+  }else if(minimum==magenta){
+    strcpy(color, "Magenta");
+  }else if(minimum==yellow){
+    strcpy(color, "Yellow");
+  }else if(minimum==red){ //RGB
+    strcpy(color, "Red");
+  }else if(minimum==green){
+    strcpy(color, "Green");
+  }else if(minimum==blue){
+    strcpy(color, "Blue");
+  }else if(minimum==orange){  //tirtiary colors and others
+    strcpy(color, "Orange");
+  }else if(minimum==kesari){
+    strcpy(color, "Kesari");
+  }else if(minimum==Lgreen){
+    strcpy(color, "Lime Green");
+  }else if(minimum==saffron){
+    strcpy(color, "Saffron");
+  }else if(minimum==pink){
+    strcpy(color, "Pink");
+  }else if(minimum==wheat){
+    strcpy(color, "Wheat");
+  }else if(minimum==gold){
+    strcpy(color, "Gold");
+  }else if(minimum==turquoise){
+    strcpy(color, "Turquoise");
+  }else if(minimum==white){ //neutrals
+    strcpy(color, "White");
+  }else if(minimum==gray){
+    strcpy(color, "Gray");
+  }else if(minimum==teal){
+    strcpy(color, "Teal");
+  }else if(minimum==brown){
+    strcpy(color, "Brown");
+  }else{strcpy(color, "!error!");}  //can only occur when logic fails silently
+  return color;
+}
+
+int avg_arr(float array_to_be_averaged[], float &avg_found){  //makes the average value for the color inputted
+  avg_found=0;
+  for(int j = 0; j<3; j++){
+    avg_found+=array_to_be_averaged[j];
+  }
+  avg_found=avg_found/3;
+  return avg_found;
+}
+int min_finder(float ARRAY[], int size, float &smallest){ //finds the minimum value stored in input array
+  smallest=255;
+  for(int k = 0; k<size; k++){
+    if(ARRAY[k]<smallest){
+      smallest=ARRAY[k];
+    }
+  }
+  return smallest;  
+}
